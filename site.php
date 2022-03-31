f7003a71dc004e8b8a143abc078bfba6cb471387
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2022-03-31 11:38:21 -0300

    Create site.php

---
+++ site.php
@@ -0,0 +1,19 @@
+<html lang="pt-br">
+
+<head>
+
+<meta charset="utf-8"/>
+<meta name="viewport" content="widht=device-width, initial-scale=1.0">
+<meta name="Description" content="pagina de formulario">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+     <title>Formulario</title>
+<?php
+      include("index.php");
+?>
+</body>
+</html>
