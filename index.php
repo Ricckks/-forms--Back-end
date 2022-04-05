ed3357dfb61cd678d78a1d23280f457b5b839a85
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2022-04-05 11:32:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
  <?php
 
-echo "Olá funcionando";
+include ("conexao.php");
+
+$nome = filter_input(INPUT_POST, 'name1', FILTER_SANITIZE_STRING);
+
+//usado apenas para ver se o campo preenchido esta sendo capturado.
+//echo "$nome";
+
+//QUERY CRIADA
+$Result_user = "INSERT INTO recebe users  (nome , created) VALUES ('$nome', NOW())";
+
+//EXECUCAO
+$Resultado_user = mysqli_query($conn,$Result_user);
 
 ?>
+
