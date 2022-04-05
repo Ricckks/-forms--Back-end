f9102233ebd05eda7be2553e0d5208eb7c710fba
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2022-04-05 11:32:34 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "user_dados";
+
+//Criar a conexao
+//$conn - variavel para conexao com o BD
+$conn = mysqli_connect ($Servidor, $usuario, $senha, $dbname);
+?>
